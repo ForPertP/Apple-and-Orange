4bb37ac82c8cc55456889a372926f5d0db18b8f3
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-10-20 09:14:01 +0900

    Update AppleandOrange.cpp

--- AppleandOrange.cpp
+++ AppleandOrange.cpp
@@ -1,3 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'countApplesAndOranges' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER s
+ *  2. INTEGER t
+ *  3. INTEGER a
+ *  4. INTEGER b
+ *  5. INTEGER_ARRAY apples
+ *  6. INTEGER_ARRAY oranges
+ */
 void countApplesAndOranges(int s, int t, int a, int b, vector<int> apples, vector<int> oranges)
 {
     int apple_count = 0;
